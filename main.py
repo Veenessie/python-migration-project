b06dbde055ef536a4f4ccc1e925b304e0f3588e8
Verity <mastersverity@gmail.com>
2024-05-09 14:44:37 +0100

    imported CSV file

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+from rich import print
+import csv 
+    
+with open('migration.csv', 'r') as file:
+    migration_file = csv.DictReader(file)       
+    for line in migration_file:
+        continent = line['continent']
+        year = line['year']
+        migration_change = line['Five-year change immigrants']
+        print(continent)
+        print(year)
+        print(migration_change)
+
+
+
+
+
